ca898b443c13c1dd845f6d78f4fec256163951eb
Thomas Levine <_@thomaslevine.com>
2014-04-03 21:05:39 +0000

    bin

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='scarsdale-property-inquiry',
+    author='Thomas Levine',
+    author_email='_@thomaslevine.com',
+    description='Inquire about Scarsdale properties',
+    url='https://github.com/tlevine/scarsdale-property-inquiry.git',
+    classifiers=[
+        'Intended Audience :: Developers',
+    ],
+    packages=['pi'],
+    scripts=['bin/scarsdale-property-inquiry'],
+    install_requires = ['requests','randua','lxml','pickle_warehouse'],
+  # tests_require = ['nose'],
+    version='0.1',
+    license='AGPL',
+)
