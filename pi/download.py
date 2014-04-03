5069477ea4cb114370b22f7638068796bd889250
Thomas Levine <_@thomaslevine.com>
2014-04-03 17:46:51 +0000

    parse home

--- pi/download.py
+++ pi/download.py
@@ -6,6 +6,8 @@ def home():
     '() -> [street_id:str]'
     response = pi.cache.home()
     html = fromstring(response.text)
+    values = html.xpath('id("dnn_ctr1381_ViewPIRPS_lstboxStreets")/option/@value')
+    return map(str, values)
 
 def street():
     'street_id -> [house_id:str]'
