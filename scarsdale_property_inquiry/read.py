39298d984a50eec025e81517a6bab768838aec3b
Thomas Levine <_@thomaslevine.com>
2014-04-19 22:39:09 +0000

    lists

--- scarsdale_property_inquiry/read.py
+++ scarsdale_property_inquiry/read.py
@@ -1,22 +1,29 @@
-import lxml.html
+from collections import OrderedDict
+from lxml.html import fromstring
 
 def info(text):
-    html = lxml.html.fromstring(text)
+    html = fromstring(text)
 
-def property_information(table):
-    return table.xpath('descendant::td[not(@style)]')
+def two_column_table(table):
+    tds = table.xpath('descendant::td[not(@style)]')
+    text_contents = [td.text_content() for td in tds]
+    keys = [key.rstrip(':') for key in text_contents[::2]]
+    values = [value.replace('\xa0', ' ') for value in text_contents[1::2]]
+    # I could read lot area as an acreage.
+    return OrderedDict(zip(keys, values))
+
+property_information = two_column_table
 
 def assessment_information(table):
-   return {}
+    return table.xpath
 
-def building_information(table):
-   return {}
+building_information = two_column_table
 
 def structure_information(table):
-   return {}
+    return table.xpath('descendant::td[not(@style)]/text()')
 
 def tax_information(table):
-   return {}
+    return {}
 
 def permits(table):
-   return {}
+    return {}
--- scarsdale_property_inquiry/test/test_read.py
+++ scarsdale_property_inquiry/test/test_read.py
@@ -4,15 +4,15 @@ import nose.tools as n
 import scarsdale_property_inquiry.read as read
 import scarsdale_property_inquiry.test.fixtures.read as fixtures
 
-def check(function_name, expected):
+def check(assertion, function_name, expected):
     html = lxml.html.fromstring(getattr(fixtures, function_name))
     observed = getattr(read, function_name)(html)
-    n.assert_dict_equal(observed, expected)
+    assertion(observed, expected)
 
 def test_sections():
-    yield check, 'property_information', {}
-    yield check, 'assessment_information', {}
-    yield check, 'building_information', {}
-    yield check, 'structure_information', {}
-    yield check, 'tax_information', {}
-    yield check, 'permits', {}
+    yield check, n.assert_dict_equal, 'property_information', {}
+    yield check, n.assert_dict_equal, 'assessment_information', {}
+    yield check, n.assert_dict_equal, 'building_information', {}
+    yield check, n.assert_list_equal, 'structure_information', {}
+    yield check, n.assert_list_equal, 'tax_information', {}
+    yield check, n.assert_list_equal, 'permits', {}
