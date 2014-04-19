b6282ef0351b7be7319ebecf349df3e4684657db
Thomas Levine <_@thomaslevine.com>
2014-04-19 22:27:17 +0000

    stubs

--- scarsdale_property_inquiry/read.py
+++ scarsdale_property_inquiry/read.py
@@ -4,4 +4,19 @@ def info(text):
     html = lxml.html.fromstring(text)
 
 def property_information(table):
-    print(table)
+    return table.xpath('descendant::td')
+
+def assessment_information(table):
+   return {}
+
+def building_information(table):
+   return {}
+
+def structure_information(table):
+   return {}
+
+def tax_information(table):
+   return {}
+
+def permits(table):
+   return {}
