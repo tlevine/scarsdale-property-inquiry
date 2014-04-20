487a8eaa4d5e27249c136892c3a4b12f6696ba00
Thomas Levine <_@thomaslevine.com>
2014-04-20 11:55:32 +0000

    excemptions

--- scarsdale_property_inquiry/main.py
+++ scarsdale_property_inquiry/main.py
@@ -37,12 +37,10 @@ def main():
             text = future.result()
             bumpy_row = read.info(text)
             if bumpy_row != None:
-                
-                if bumpy_row['excemptions'] != []:
+                if bumpy_row.get('excemptions', []) != []:
                     for excemption in bumpy_row['excemptions']:
                         excemption['property_number'] = bumpy_row['Property Number']
                         db['excemptions'].upsert(excemption, ['property_number'])
-
                 flat_row = read.flatten(bumpy_row)
                 if flat_row != None and 'property_number' in flat_row:
                     try:
