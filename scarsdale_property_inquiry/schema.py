dc7cc24373e7834b94111bb308a05d5d5d0ad7a1
Thomas Levine <_@thomaslevine.com>
2014-04-20 23:35:01 +0000

    text -> varchar

--- scarsdale_property_inquiry/schema.py
+++ scarsdale_property_inquiry/schema.py
@@ -1,7 +1,7 @@
 properties = '''
 CREATE TABLE IF NOT EXISTS properties (
   -- Property Information
-  property_number TEXT NOT NULL,
+  property_number VARCHAR(30) NOT NULL,
   curr_owner TEXT,
   wetlands TEXT,
   address TEXT,
